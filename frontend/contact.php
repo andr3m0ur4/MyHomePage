ff6a114524717c392d13ba26a49f29d9192ccaea
André Moura <andre.benedicto@etec.sp.gov.br>
2020-07-17 10:54:13 -0300

    email

--- frontend/contact.php
+++ frontend/contact.php
@@ -1,32 +1,30 @@
 <?php
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
 
-$mail_to = 'newthinks@live.com';
-$subject = 'Message from a site visitor '.$field_name;
+	$field_name = $_POST['cf_name'];
+	$field_email = $_POST['cf_email'];
+	$field_message = $_POST['cf_message'];
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
+	$mail_to = 'andre.benedicto@etec.sp.gov.br';
+	$subject = 'Mensagem do visitante do site ' . $field_name;
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+	$body_message = 'Do: '.$field_name."\n";
+	$body_message .= 'E-mail: '.$field_email."\n";
+	$body_message .= 'Messagem: '.$field_message;
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
+	$headers = 'From: '.$field_email."\r\n";
+	$headers .= 'Reply-To: '.$field_email."\r\n";
 
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'index.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to newthinks@live.com');
-		window.location = 'index.html';
-	</script>
-<?php
-}
-?>
+	$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+	if ($mail_status) : ?>
+		<script language="javascript" type="text/javascript">
+			alert('Obrigado pela mensagem. Irei entrar em contato em breve.')
+			window.location = '/'
+		</script>
+	<?php else : ?>
+		<script language="javascript" type="text/javascript">
+			alert('Mensagem falhou. Por favor, envie um e-mail para andre.benedicto@etec.sp.gov.br')
+			window.location = '/'
+		</script>
+	<?php
+	endif;
